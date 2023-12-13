76844ae667082b9e50515b19295947f55b97817e
Rob Fischer <robfischer1@gmail.com>
2023-12-13 04:22:41 -0500

    - Working TFRecords, again.

--- wrecksys/data/process.py
+++ wrecksys/data/process.py
@@ -9,7 +9,6 @@ from collections import namedtuple
 import numpy as np
 import pandas as pd
 from tqdm.auto import tqdm
-from typing_extensions import Self
 
 from wrecksys.utils import import_tensorflow
 tf, keras = import_tensorflow()
@@ -18,10 +17,10 @@ logger = logging.getLogger(__name__)
 UserHistory = namedtuple("UserHistory", "history books ratings")
 UserContext = namedtuple("UserContext", "ids ratings label")
 
-RECORD_TEMPLATE = 'goodreads{:02}.tfrecord'
 RECORD_COUNT = 'WRECKSYS_ROWS'
 
-class BaseDataset(object):
+class SequentialDataset(object):
+    _class_logger = logging.getLogger(__name__).getChild(__qualname__)
     def __init__(self,
                  input_file: str | os.PathLike,
                  output_file: str | os.PathLike,
@@ -32,18 +31,20 @@ class BaseDataset(object):
         self.output_file = pathlib.Path(output_file)
         self.min_length = min_length
         self.max_length = max_length
+        self.size = -1
+        self._class_logger.debug(f"Input file: {self.input_file}")
+        self._class_logger.debug(f"Output file: {self.output_file}")
 
-        self.data = None
+    def build(self):
+        raise NotImplementedError()
 
-    def clear(self) -> None:
-        self.data = None
+    def delete(self) -> None:
+        self.output_file.unlink(missing_ok=True)
+        self._class_logger.info(f"Deleted {self.output_file}.")
 
-    def to_feather(self, file: str | os.PathLike) -> None:
-        context_ids, context_ratings, label_ids = self.data
-        df = pd.DataFrame.from_dict({'context_id': context_ids,
-                                       'context_rating': context_ratings,
-                                       'label_id': label_ids})
-        df.to_feather(pathlib.Path(file))
+    def load(self):
+        raise NotImplementedError()
+
 
     def _from_source(self):
         context_ids = []
@@ -66,8 +67,12 @@ class BaseDataset(object):
                     context_ratings.extend(user_context_ratings)
                     label_ids.extend(user_label_ids)
                 timeline_progress.update(1)
+                sys.stdout.flush()
 
-        self.data = context_ids, context_ratings, label_ids
+        del df
+        n_rows = len(label_ids)
+        self._class_logger.info(f"Successfully created {n_rows} training examples.")
+        return context_ids, context_ratings, label_ids
 
     def _build_user_context(
             self,
@@ -81,15 +86,129 @@ class BaseDataset(object):
         for label in range(1, len(books)):
             pos = max(0, label - self.max_length)
             if (label - pos) >= self.min_length:
-                context_id = np.array(books[pos:label], dtype=np.int32).reshape(10)
-                context_rating = np.array(ratings[pos:label], dtype=np.float32).reshape(10)
+                context_id = np.array(books[pos:label], dtype=np.int32)
+                context_id.resize(self.max_length)
+                context_rating = np.array(ratings[pos:label], dtype=np.float32)
+                context_rating.resize(self.max_length)
                 label_id = np.array([books[label]], dtype=np.int32)
 
-                user_context_ids.append(np.array(context_id, dtype=np.int32))
-                user_context_ratings.append(np.array(context_rating, dtype=np.float32))
-                user_label_ids.append(np.array(label_id, dtype=np.int32))
+                user_context_ids.append(context_id)
+                user_context_ratings.append(context_rating)
+                user_label_ids.append(label_id)
 
         return user_context_ids, user_context_ratings, user_label_ids
+
+class FeatherDataset(SequentialDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def build(self) -> None:
+        context_ids, context_ratings, label_ids = self._from_source()
+        self.size = len(label_ids)
+        df = pd.DataFrame.from_dict({'context_id': context_ids,
+                                       'context_rating': context_ratings,
+                                       'label_id': label_ids})
+        del context_ids, context_ratings, label_ids
+        df.to_feather(self.output_file)
+        self._class_logger.info(f"Saved {self.output_file.name} in {self.output_file.parent}")
+
+    def load(self) -> tf.data.Dataset:
+        df = pd.read_feather(self.output_file)
+        self._class_logger.info(f"Loaded {self.output_file.name} from {self.output_file.parent}")
+        data = {col: df[col].to_list() for col in df}
+        del df
+
+        d = tf.data.Dataset.from_tensor_slices((data, data['label_id']))
+        return d
+
+class NumpyDataset(SequentialDataset):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def build(self):
+        context_ids, context_ratings, label_ids = self._from_source()
+        self.size = len(label_ids)
+        with open(self.output_file, 'wb') as f:
+            np.savez_compressed(f,
+                                context_id=np.array(context_ids),
+                                context_rating=np.array(context_ratings),
+                                label_id=np.array(label_ids)
+                                )
+
+    def load(self):
+        with open(self.output_file, 'rb') as f:
+            npz = np.load(f)
+            labels = npz['label_id']
+            logger.debug(f"Loaded {self.output_file.name}")
+            return tf.data.Dataset.from_tensor_slices((dict(npz), labels))
+
+
+class TFRecordsDataset(SequentialDataset):
+    def __init__(self, num_shards, *args, **kwargs):
+        self.num_shards = num_shards
+        super().__init__(*args, **kwargs)
+        self.output_file = self.output_file.parent
+        self.file_template = 'goodreads{:02}.tfrecord'
+
+    def build(self):
+        if self.num_shards == len([f for f in self.output_file.glob('*.tfrecord')]):
+            return
+
+        records = []
+        file_count = 0
+
+        df = pd.read_feather(self.input_file, dtype_backend='pyarrow')
+        self.size = len(df)
+        cutoff = self.size // self.num_shards
+
+        with tqdm(total=len(df.user_id.unique()),
+                  desc="Building timelines ",
+                  file=sys.stdout,
+                  unit=' users') as timeline_progress:
+            for _, group in df.groupby('user_id', observed=True):
+                books: list = group['work_id'].tolist()
+                ratings: list = group['rating'].tolist()
+
+                if len(books) >= self.min_length:
+                    user_examples = self._build_user_context(books, ratings)
+                    records.extend(user_examples)
+
+                if len(records) >= cutoff:
+                    _write_tf_records(records,
+                                      str(self.output_file /
+                                          self.file_template.format(file_count)))
+                    file_count += 1
+                    records = []
+
+                timeline_progress.update(1)
+                sys.stdout.flush()
+
+            if records:
+                _write_tf_records(records,
+                                  str(self.output_file /
+                                      self.file_template.format(file_count)))
+                file_count += 1
+                del df, records
+
+        self._class_logger.info(f"Successfully created {self.size} training examples in {file_count} files.")
+
+    def _build_user_context(self, books: list[int], ratings: list[int]) -> list[tf.train.Example]:
+
+        examples = []
+
+        for label in range(1, len(books)):
+            pos = max(0, label - self.max_length)
+            if (label - pos) >= self.min_length:
+                context_id = np.array(books[pos:label], dtype=np.int32)
+                context_id.resize(self.max_length)
+                context_rating = np.array(ratings[pos:label], dtype=np.float32)
+                context_rating.resize(self.max_length)
+                label_id = np.array([books[label]], dtype=np.int32)
+
+                example = _create_tf_example(context_id, context_rating, label_id)
+                examples.append(example)
+
+        return examples
 
 
 
@@ -133,29 +252,6 @@ def _create_training_data_worker(source: str,
     save_fn(contexts, destination, num_shards)
 
 
-def _contexts_to_numpy(contexts: list[UserContext], output_file: str, num_shards=None) -> None:
-    output_file = pathlib.Path(output_file)
-    context_ids = []
-    context_ratings = []
-    label_ids = []
-
-    for context in contexts:
-        context_id = np.array(context.ids, dtype=np.int32)
-        context_ids.append(context_id)
-
-        context_rating = np.array(context.ratings, dtype=np.float32)
-        context_ratings.append(context_rating)
-
-        label_id = np.array(context.label, dtype=np.int32)
-        label_ids.append(label_id)
-
-    with open(output_file, 'wb') as f:
-        np.savez_compressed(f,
-                            context_id=np.array(context_ids),
-                            context_rating=np.array(context_ratings),
-                            label_id=np.array(label_ids)
-                            )
-
 def _contexts_to_examples(contexts: list[UserContext], output_file: str, num_shards: int) -> None:
     dataset_dir = pathlib.Path(output_file).parent.resolve()
     logger.debug("Generating examples")
@@ -181,59 +277,23 @@ def _create_tf_example(context_id, context_rating, label_id):
 
 
 def _write_tf_records(tf_examples, filename):
-    logger.debug(f"Writing {len(tf_examples)} to {filename}")
+    logger.debug(f"Writing {len(tf_examples):,} records to {filename}")
     with tf.io.TFRecordWriter(filename) as f:
         for example in tf_examples:
             f.write(example.SerializeToString())
 
 
-def build_timelines(df: pd.DataFrame) -> list[UserHistory]:
-    timelines = []
-
-    with tqdm(total=len(df.user_id.unique()),
-              desc="Building timelines ",
-              file=sys.stdout,
-              unit=' users') as timeline_progress:
-        for _, group in df.groupby('user_id', observed=True):
-            books: list = group['work_id'].tolist()
-            ratings: list = group['rating'].tolist()
-            size: int = len(books)
-            user_history = UserHistory(size, books, ratings)
-            timelines.append(user_history)
-            timeline_progress.update(1)
-
-    return timelines
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG)
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+    logger.debug("Huh?")
 
 
-def build_contexts(timeline: list[UserHistory], min_length: int, max_length: int) -> list[UserContext]:
-    contexts = []
-    with tqdm(total=len(timeline),
-              desc="Building contexts ",
-              file=sys.stdout,
-              unit=' timelines') as conversion_progress:
-        for user in timeline:
-            if user.history < min_length:
-                conversion_progress.update(1)
-                continue
-            user_contexts = build_user_context(user, min_length, max_length)
-            contexts.extend(user_contexts)
-            conversion_progress.update(1)
-    return contexts
+    test_ds = TFRecordsDataset(input_file='../../data/clean/ratings.feather',
+                               output_file='../../data/dataset/file.format',
+                               min_length=3,
+                               max_length=10,
+                               num_shards=10)
 
-
-
-def build_user_context(user: UserHistory, min_length: int, max_length: int) -> list[UserContext]:
-    user_contexts = []
-
-    for label in range(1, len(user.books)):
-        pos = max(0, label - max_length)
-        if (label - pos) >= min_length:
-            context_id = user.books[pos:label]
-            context_rating = user.ratings[pos:label]
-            label_id = [user.books[label]]
-            while len(context_id) < max_length:
-                context_id.append(0)
-                context_rating.append(0)
-            user_contexts.append(UserContext(context_id, context_rating, label_id))
-    return user_contexts
-
+    test_ds.build()
